401efa95aa253caed72344edb77a015f98ab58fe
Ivan Perez <ivan_psh@hotmail.com>
2023-03-20 16:22:58 +0100

    Update

--- app/src/main/java/es/ieslavereda/myapplication/MainActivity.java
+++ app/src/main/java/es/ieslavereda/myapplication/MainActivity.java
@@ -1,16 +1,26 @@
 package es.ieslavereda.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TableLayout myTableLayout;
+    private Spinner spinner;
+    private TextView textView;
+    private ConstraintLayout myTableLayout;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -18,15 +28,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myTableLayout = findViewById(R.id.myTableLayout);
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Joaquin","Alonso Saiz"));
+        usuarios.add(new Usuario("Pedro","Garcia Lopez"));
 
-        TextView textView = new TextView(getApplicationContext());
-        textView.setText("TextView generado por código");
 
-        TableRow row = new TableRow(getApplicationContext());
-        row.addView(textView);
+        spinner = findViewById(R.id.spinner);
+        myTableLayout = findViewById(R.id.constraintLayout);
+        textView = findViewById(R.id.textView);
 
-        myTableLayout.addView(row);
+//        TextView textView = new TextView(getApplicationContext());
+//        textView.setText("TextView generado por código");
+
+
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sexo);
+        ArrayAdapter<Usuario> myAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,usuarios);
+        spinner.setAdapter(myAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                textView.setText(usuarios.get(i).toString());
+//                adapterView.getItemIdAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
     }
 }
